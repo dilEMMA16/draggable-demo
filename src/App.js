d511f454368597d45dd19fa9d79497ddfdb72b4d
Emma <emma.schreifels@gmail.com>
2022-06-24 15:48:09 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Board from './components/Board';
+import Cards from './components/Cards';
+import {Card} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <main className="flexbox"> 
+        <Board id="board-1" className="board">
+          <h3>start</h3>
+          <Cards id="card-1" className="card" draggable="true">
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+          <Cards id="card-1-1" className="card" draggable="true">
+          <Card>
+              <Card.Body>
+              <Card.Title>Card 2</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+        </Board>
+        <Board id="board-2" className="board">
+          <h3>stop</h3>
+          <Cards id="card-2" className="card" draggable="true">
+          <Card>
+              <Card.Body>
+              <Card.Title>Card 3</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+          <Cards id="card-2-1" className="card" draggable="true">
+          <Card>
+              <Card.Body>
+              <Card.Title>Card 4</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+        </Board>
+        <Board id="board-3" className="board">
+          <h3>continue</h3>
+          <Cards id="card-3" className="card" draggable="true">
+          <Card>
+              <Card.Body>
+              <Card.Title>Card 5</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+          <Cards id="card-3-1" className="card" draggable="true">
+          <Card>
+              <Card.Body>
+              <Card.Title>Card 6</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Cards>
+        </Board>
+      </main>
+    </div>
+  );
+}
+
+export default App;
